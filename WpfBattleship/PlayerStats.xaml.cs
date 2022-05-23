ac69b648e0df683bf3ee15baf0a1c71a3c34bab8
Matchbox <dawnyitrai@gmail.com>
2022-05-23 15:14:35 +0200

    Player Stats frontend

--- WpfBattleship/PlayerStats.xaml.cs
+++ WpfBattleship/PlayerStats.xaml.cs
@@ -20,6 +20,22 @@ namespace NationalInstruments
         public PlayerStats()
         {
             InitializeComponent();
+            SinglePlayerDatabase johnSmith = new SinglePlayerDatabase();
+            johnSmith.PlayerName = "John Smith";
+            johnSmith.Winner = "AI";
+            johnSmith.NumberOfRounds = 22;
+            johnSmith.PlayerHits = 10;
+
+            SinglePlayerDataGridXAML.Items.Add(johnSmith);
+        }
+
+        public class SinglePlayerDatabase
+        {
+            public string PlayerName { get; set; }
+            public string Winner { get; set; }
+            public int NumberOfRounds { get; set; }
+            public int PlayerHits { get; set; }
+
         }
     }
 }
