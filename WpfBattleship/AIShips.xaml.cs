e2d5668691b870efedb1e2f0ce1c1be9314355f8
vargalz <v.levi0218@gmail.com>
2022-06-01 14:11:36 +0200

    Show enemy ships feature improved

--- WpfBattleship/AIShips.xaml.cs
+++ WpfBattleship/AIShips.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,11 +21,41 @@ namespace NationalInstruments
         public AIShips()
         {
             InitializeComponent();
+
+            int count = 1;
+
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    Button myControl1 = new Button();
+                    myControl1.Content = count.ToString(new CultureInfo("hu-HU"));
+                    myControl1.Name = "EnemyButton" + count.ToString(new CultureInfo("hu-HU"));
+                    myControl1.IsHitTestVisible = false;
+                    Grid.SetColumn(myControl1, j);
+                    Grid.SetRow(myControl1, i);
+
+                    if (enemyShipList.Contains(myControl1.Name))
+                    {
+                        myControl1.Background = myControl1.Background == Brushes.Red
+                    ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD"))
+                    : Brushes.Red;
+                    }
+
+                    Boardd.Children.Add(myControl1);
+
+                    count++;
+                }
+            }
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private static readonly string[] enemyShips = new string[]
         {
-
-        }
+            "EnemyButton2", "EnemyButton10", "EnemyButton51", "EnemyButton47",
+            "EnemyButton6", "EnemyButton7", "EnemyButton65", "EnemyButton66", "EnemyButton73", "EnemyButton83",
+            "EnemyButton24", "EnemyButton34", "EnemyButton44", "EnemyButton28", "EnemyButton29", "EnemyButton30",
+            "EnemyButton59", "EnemyButton69", "EnemyButton79", "EnemyButton89"
+        };
+        List<string> enemyShipList = new List<string>(enemyShips);
     }
 }
